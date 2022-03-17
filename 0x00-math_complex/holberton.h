fe5308e3def27a4db970b7bdac848d6d743b4ca2
SebastianBlandon <blandonodz@gmail.com>
2022-03-17 15:01:19 -0500

    This file contains info about holberton.h

--- 0x00-math_complex/holberton.h
+++ 0x00-math_complex/holberton.h
@@ -6,7 +6,7 @@
 #include <ctype.h>
 #include <math.h>
 /**
- * struct op - Struct op
+ * struct num_complex - Struct complex numbers
  *
  * @re: The operator
  * @im: The function associated
@@ -25,6 +25,6 @@ void addition(complex c1, complex c2, complex *c3);
 void substraction(complex c1, complex c2, complex *c3);
 void multiplication(complex c1, complex c2, complex *c3);
 void division(complex c1, complex c2, complex *c3);
-void complex_from_mod_arg (double m, double arg, complex *c3);
+void complex_from_mod_arg(double m, double arg, complex *c3);
 
 #endif /* HOLBERTON_H_ */
