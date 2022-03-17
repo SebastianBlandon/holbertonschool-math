46d6a83144ed98f623381015965f3e534d0a439d
SebastianBlandon <blandonodz@gmail.com>
2022-03-17 18:11:04 -0500

    This file contains info about 0-display.c

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -1,6 +1,26 @@
 #include "holberton.h"
 
 /**
+ * print_double - print of double value
+ * @b: double input.
+ * Return: nothing.
+ */
+void print_double(double b)
+{
+	double f = 10, parte_d_b = b * f;
+
+	printf("%d", (int)b);
+	if ((int)parte_d_b % 10 != 0)
+	{
+		printf(".");
+	}
+	while ((int)parte_d_b % 10 != 0)
+	{
+		printf("%d", (int)parte_d_b % 10);
+		parte_d_b = parte_d_b * f;
+	}
+}
+/**
  * display_complex_number - displays the complex numbers,
  *  followed by a new line.
  * @c: complex struct input.
@@ -8,7 +28,7 @@
  */
 void display_complex_number(complex c)
 {
-	printf("%.0f ", c.re);
+	print_double(c.re);
 	if (c.im != 0)
 	{
 		if (c.im < 0)
@@ -19,7 +39,7 @@ void display_complex_number(complex c)
 		else
 			printf("+ ");
 		if (c.im != 1)
-			printf("%.0f", c.im);
+			print_double(c.im);
 		printf("i");
 	}
 	printf("\n");
