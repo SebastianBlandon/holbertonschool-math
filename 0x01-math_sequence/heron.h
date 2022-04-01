107b5dae69aadaac27c8fda29beb56890bde8043
SebastianBlandon <blandonodz@gmail.com>
2022-03-31 23:05:55 -0500

    heron.h ready

---
+++ 0x01-math_sequence/heron.h
@@ -0,0 +1,23 @@
+#ifndef _HERON_H_
+#define _HERON_H_
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * struct s_cell - singly linked list
+ * @elt: long float element
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct s_cell
+{
+	double elt;
+	struct s_cell *next;
+} t_cell;
+
+t_cell *heron(double p, double x0);
+
+#endif /* _HERON_H_ */
