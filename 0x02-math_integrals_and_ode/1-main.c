9eea77cef35835b8a95aeeb6b9aa85837560c4b2
SebastianBlandon <blandonodz@gmail.com>
2022-04-22 11:35:21 -0500

    [UPDATED] Simpson Method

--- 0x02-math_integrals_and_ode/1-main.c
+++ 0x02-math_integrals_and_ode/1-main.c
@@ -17,4 +17,5 @@ int main()
     simpon_integral = simpson(a, b, steps);
     printf("The Simpson approximation of the Integral is: %.9f\n", simpon_integral);
     printf("The exact value is:                           %.9f\n", 3.1415926535 / 4);
+	return (0);
 }
--- 0x02-math_integrals_and_ode/1-simpson.c
+++ 0x02-math_integrals_and_ode/1-simpson.c
@@ -7,7 +7,7 @@ double x(double a, double delta_x, int i)
 
 double f(double x)
 {
-	return (1 / (1 + x));
+	return (1 / (1 + x * x));
 }
 
 /**
@@ -21,17 +21,14 @@ double simpson(double a, double b, int steps)
 {
 	int j, n = steps;
 	double result = 0, delta_x = (b - a) / n;
-	double zero = 0, two = 0, four = 0, ene = 0;
 
-	zero += f(x(a, delta_x, 0));
-	for(j = 1; j < n - 1; j++)
-		two += f(x(a, delta_x, 2 * j));
-	two *= 2;
-	for (j = 1; j < n; j++)
-		four += f(x(a, delta_x, 2 * j - 1));
-	four *= 4;
-	ene += f(x(a, delta_x, n));
-	result = zero + two + four + ene;
+	result += f(x(a, delta_x, 0));
+	for(j = 1; j <= n / 2; j++)
+	{
+		result += 4 * f(x(a, delta_x, 2 * j - 1));
+		result += 2 * f(x(a, delta_x, 2 * j));
+	}
+	result += f(x(a, delta_x, n));
 	result *= delta_x / 3;
 	return (result);
 }
