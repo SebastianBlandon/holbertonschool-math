07f2ea53399601bbd69e3e26962758ebece711df
SebastianBlandon <blandonodz@gmail.com>
2022-04-21 20:49:31 -0500

    [UPDATED] Rectangle Method

--- 0x02-math_integrals_and_ode/0-rectangle.c
+++ 0x02-math_integrals_and_ode/0-rectangle.c
@@ -10,13 +10,14 @@
 double rectangle_method(double a, double b, int steps)
 {
 	int i = 0, n = steps + 1;
-	double result = 0, delta_x = ((b - a) / n), x_previous, x_present;
+	double result = 0, delta_x = ((b - a) / n), x_previous, x_present, x_i;
 
 	x_previous = (a + (i * delta_x)), i++;
 	while (i <= n)
 	{
 		x_present = (a + (i * delta_x));
-		result += (1 / (1 + pow((x_previous + x_present) / 2, 2)));
+		x_i = (x_previous + x_present) / 2;
+		result += (1 / (1 + (x_i * x_i)));
 		x_previous = x_present, i++;
 	}
 	result *= delta_x;
