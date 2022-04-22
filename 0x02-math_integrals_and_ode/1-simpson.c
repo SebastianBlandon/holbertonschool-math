59898e7ffdedde09a4b80c0636d1b967315b6f7f
SebastianBlandon <blandonodz@gmail.com>
2022-04-22 11:12:16 -0500

    [UPDATED] Rectangle Method

--- 0x02-math_integrals_and_ode/1-simpson.c
+++ 0x02-math_integrals_and_ode/1-simpson.c
@@ -1,4 +1,14 @@
-#include "rectangle.h"
+#include "simpson.h"
+
+double x(double a, double delta_x, int i)
+{
+	return (a + i * delta_x);
+}
+
+double f(double x)
+{
+	return (1 / (1 + x));
+}
 
 /**
  * simpson - Simpson Method integrals.
@@ -9,13 +19,19 @@
  */
 double simpson(double a, double b, int steps)
 {
-	double result = 0;
-	int i = 1, n = steps;
+	int j, n = steps;
+	double result = 0, delta_x = (b - a) / n;
+	double zero = 0, two = 0, four = 0, ene = 0;
 
-	while (i < steps)
-	{
-		result += ((b - a) / n) * (1 / (1 + pow((a + (i * (b - a) / n)), 2)));
-		i++;
-	}
+	zero += f(x(a, delta_x, 0));
+	for(j = 1; j < n - 1; j++)
+		two += f(x(a, delta_x, 2 * j));
+	two *= 2;
+	for (j = 1; j < n; j++)
+		four += f(x(a, delta_x, 2 * j - 1));
+	four *= 4;
+	ene += f(x(a, delta_x, n));
+	result = zero + two + four + ene;
+	result *= delta_x / 3;
 	return (result);
 }
