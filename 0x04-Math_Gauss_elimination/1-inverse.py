34c17a26b7443c8ebd4d4d3c352a9b17c524235b
SebastianBlandon <blandonodz@gmail.com>
2022-05-06 09:47:52 -0500

    [UPDATED] 1-inverse.py

--- 0x04-Math_Gauss_elimination/1-inverse.py
+++ 0x04-Math_Gauss_elimination/1-inverse.py
@@ -7,12 +7,12 @@ if __name__ == "__main__":
 
     def inverse(A):
         return gaussian_elimination(A, np.identity(len(A)))
-    A = np.zeros((4, 4))
-    A[2, 0]=1
-    A[0, 1] = 1
-    A[1, 3] =1
-    A[3, 2] = 1
-    print("The matrix A:\n",A)
-    print("The inverse of the matrix A:\n",inverse(A))
-    print("The identity matrix (The matrix A x The inverse of the matrix A): \n",A @ inverse(A) )
 
+    A = np.zeros((4, 4))
+    A[2, 0] = 1
+    A[0, 1] = 1
+    A[1, 3] = 1
+    A[3, 2] = 1
+    print("The matrix A:\n", A)
+    print("The inverse of the matrix A:\n", inverse(A))
+    print("The identity matrix (The matrix A x The inverse of the matrix A): \n", A @ inverse(A))
