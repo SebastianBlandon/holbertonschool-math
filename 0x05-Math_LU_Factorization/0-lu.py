f64deeda7ae07010fee5c90d7023b95bb49159d2
SebastianBlandon <blandonodz@gmail.com>
2022-05-10 21:17:27 -0500

    [UPDATED] 0-lu.py

--- 0x05-Math_LU_Factorization/0-lu.py
+++ 0x05-Math_LU_Factorization/0-lu.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
-import scipy
-import scipy.linalg as solve
+import numpy as np
 def lu(A):
-    _, L, U = solve.lu(A)
+    size = len(A)
+    L = np.zeros([size, size])
+    U = np.zeros([size, size])
+    U[0] = A[0]
+    for i in range(size):
+        for j in range(size):
+            if (i == j):
+                L[i, j] = 1
+            if (i < j):
+                fact = (A[j, i] / A[i, i])
+                L[j, i] = fact
+                for k in range(size):
+                    A[j, k] = A[j, k] - (fact * A[i, k])
+                    U[j, k] = A[j, k]
     return L, U
