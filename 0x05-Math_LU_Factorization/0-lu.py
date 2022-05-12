48cbb3e1ec9290364862646037b96c15b2b1a9e6
SebastianBlandon <blandonodz@gmail.com>
2022-05-12 16:51:30 -0500

    [UPDATED] 0-lu.py

--- 0x05-Math_LU_Factorization/0-lu.py
+++ 0x05-Math_LU_Factorization/0-lu.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
+
 import numpy as np
+
 def lu(A):
     size = len(A)
-    L = np.zeros([size, size])
-    U = np.zeros([size, size])
+    L = np.eye(size, dtype=np.double)
+    U = np.array(A, np.double)
     U[0] = A[0]
     for i in range(size):
-        for j in range(size):
-            if (i == j):
-                L[i, j] = 1
-            if (i < j):
-                fact = (A[j, i] / A[i, i])
-                L[j, i] = fact
-                for k in range(size):
-                    A[j, k] = A[j, k] - (fact * A[i, k])
-                    U[j, k] = A[j, k]
+        fact = U[i+1:, i] / U[i, i]
+        L[i+1:, i] = fact
+        U[i+1:,] -= fact[:, np.newaxis] * U[i]
     return L, U
