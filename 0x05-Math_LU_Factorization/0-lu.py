59db20a1fc4be1593df57bca3f8546989d30b9d0
SebastianBlandon <blandonodz@gmail.com>
2022-05-12 16:52:38 -0500

    [UPDATED] 0-lu.py

--- 0x05-Math_LU_Factorization/0-lu.py
+++ 0x05-Math_LU_Factorization/0-lu.py
@@ -6,7 +6,6 @@ def lu(A):
     size = len(A)
     L = np.eye(size, dtype=np.double)
     U = np.array(A, np.double)
-    U[0] = A[0]
     for i in range(size):
         fact = U[i+1:, i] / U[i, i]
         L[i+1:, i] = fact
