e02d565667d289bd4335b68655beaa740b87674c
SebastianBlandon <blandonodz@gmail.com>
2022-05-12 16:51:50 -0500

    [UPDATED] 1-plu.py

---
+++ 0x05-Math_LU_Factorization/1-plu.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+def plu(A):
+    size = len(A)
+    P = np.eye(size, dtype=np.double)
+    L = np.eye(size, dtype=np.double)
+    U = np.array(A, np.double)
+    for i in range(size):
+        for j in range(i, size):
+            if ~np.isclose(U[i, i], 0.0):
+                break
+            U[[j, j+1]] = U[[j+1, j]]
+            P[[j, j+1]] = P[[j+1, j]]
+        fact = U[i+1:, i] / U[i, i]
+        L[i+1:, i] = fact
+        U[i+1:,] -= fact[:, np.newaxis] * U[i]
+    return P, L, U
